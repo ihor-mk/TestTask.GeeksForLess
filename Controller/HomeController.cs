b19ce66ace907e1ab6bc75d898f6a2c021afa961
Ihor Myroniuk <ihor.myroniuk@hotmail.com>
2022-11-03 17:25:06 +0200

    Add parsing logic

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -1,7 +1,8 @@
 using GFL.TestTask.Database;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GFL.TestTask.Controllers{
+namespace GFL.TestTask.Controllers
+{
     public class HomeController : Controller
     {
         private AppDbContext _context;
@@ -9,16 +10,46 @@ namespace GFL.TestTask.Controllers{
         {
             _context = context;
         }
-             public IActionResult Index()
-             {
-                return View();
-             }  
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-            [HttpGet("{id}")]
-             public IActionResult Folder(int id)
-             {
-                var folder = _context.Folders.FirstOrDefault(x => x.Id == id);
+/*         [HttpGet("{id}")]
+        public IActionResult Folder(int id)
+        {
+            var folder = _context.Folders.FirstOrDefault(x => x.Id == id);
+            return View(id);
+        } */
+
+        [HttpGet("{fullPath}")]
+        public IActionResult Folder(string fullPath)
+        {
+            int id = 2;
+            var subs = fullPath.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+            if (subs.Length > 0)
+            {
+                var currentFolder = _context.Folders.Where(x => x.ParentId == null).FirstOrDefault();
+
+                if (subs[0] != currentFolder.Title)
+                    return View(id);
+
+                for (int i = 1; i < subs.Length; i++)
+                {
+                    currentFolder = _context.Folders
+                        .Where(x => x.ParentId == currentFolder.Id && x.Title == subs[i])
+                        .FirstOrDefault();
+
+                    if (currentFolder is null)
+                        return View(id);
+                }
+                return View(currentFolder.Id);
+            }
+            else 
+            {
                 return View(id);
-             }
-     }
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,17 +4,25 @@ using GFL.TestTask.Database;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddMvc( option => 
+builder.Services.AddMvc(option =>
     option.EnableEndpointRouting = false
     );
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-    
+
 
 
 var app = builder.Build();
 
-app.UseMvcWithDefaultRoute();
+//app.UseMvcWithDefaultRoute();
+
+app.UseMvc(routes =>
+{
+    routes.MapRoute(
+           name: "folderPath",
+           template: "{*fullPath}"
+       );
+});
 app.UseStaticFiles();
 app.Run();
